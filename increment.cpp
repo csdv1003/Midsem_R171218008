6a27c9c168c408f79d737825d923c1920b721af7
Ajay Tyagi <ajaytyagi0209@gmail.com>
2019-03-18 10:31:17 +0530

    a incremented, b decremented

---
+++ increment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;      //declared two variables
+	cout<<"enter a: ";
+	cin>>a;
+	cout<<"enter b: ";
+	cin>>b;
+	a++;            //a incremented 
+	b--;            //b decremented
+	cout<<a<<endl<<b;
+}
+
